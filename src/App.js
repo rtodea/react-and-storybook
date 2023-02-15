d9860864e288bfda540167348b9cbeccb5a5a6d5
Robert Todea <robert.todea@mai.ai>
2023-02-15 06:55:15 +0200

    Add tailwind

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
